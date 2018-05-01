d09bb44e0c2daa7765075391c6e28d3a51c903d1
HGalitska <h.galitskaya@gmail.com>
2018-05-01 20:37:50 +0300

    ✅🚀 created client app, connected to server API

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
